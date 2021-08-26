c81e5dccb4c9444e98c5f667d854a474f656ded2
Chris Young <chrisyoung0101@gmail.com>
2021-08-26 18:53:39 -0500

    Add Button.js. Add Click event.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click');
+  };
+
   return (
-    <header>
-      <h1>Traversy Task Tracker</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Hello" onClick={onClick} />
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: 'Traversy Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
