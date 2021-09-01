d3b0c744948aea032f0b0d596fd204f4576fce16
Chris Young <chrisyoung0101@gmail.com>
2021-09-01 08:02:10 -0500

    Add state to AddTask.js. Add value & onChange to task input.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="Add Task" />
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label>Day & Time</label>
